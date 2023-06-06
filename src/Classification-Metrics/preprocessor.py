768e7e886ce4492c21415e5e6ff1d9b9b4f91ca9
Ciaran Casey <ccasee@hotmail.co.uk>
2023-06-06 08:43:46 +0100

    Collated preprocessor functions into a class

--- src/Classification-Metrics/preprocessor.py
+++ src/Classification-Metrics/preprocessor.py
@@ -1,5 +1,4 @@
-import string
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize
 import re
 
 # Reading in text to analyse from file
@@ -7,18 +6,26 @@ with open(r'C:\Users\ccase\Desktop\Dissertation\Examples of Text\Human\Human_Har
     text_to_analyse = f.read()
 
 
-def split_text_into_sentences(input_text):
-    sentences = sent_tokenize(input_text, language='English')
-    # Ensures that each tokenized sentence contains at least one valid word
-    filtered_sentences = [sentence for sentence in sentences if re.search(r'\w', sentence)]
-    return filtered_sentences
+class TextPreprocessor:
 
+    def __init__(self):
+        pass
 
-def calculate_num_of_characters(input_text):
-    """Calculates the number of characters in an input text"""
-    return len(input_text)
+    @staticmethod
+    def split_text_into_sentences(input_text):
+        """Separates a piece of text into sentences."""
+        sentences = sent_tokenize(input_text, language='English')
+        # Ensures that each tokenized sentence contains at least one valid word
+        processed_sentences = [sentence for sentence in sentences if re.search(r'\w', sentence)]
+        return processed_sentences
 
+    @staticmethod
+    def calculate_num_of_characters(input_text):
+        """Calculates the number of characters in an input text"""
+        return len(input_text)
 
-def remove_new_line_characters(input_text):
-    processed_text = input_text.replace('\n', '')
-    return processed_text
+    @staticmethod
+    def remove_new_line_characters(input_text):
+        """Removes new line characters from a piece of text."""
+        processed_text = input_text.replace('\n', '')
+        return processed_text
