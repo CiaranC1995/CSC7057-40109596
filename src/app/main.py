8df84d49c26b7c57c64fca6c4617acd584595f5f
Ciaran Casey <ccasee@hotmail.co.uk>
2023-06-12 16:42:05 +0100

    Changed function to a class

--- src/app/main.py
+++ src/app/main.py
@@ -9,38 +9,44 @@ with open(r'C:\Users\ccase\Desktop\Dissertation\Examples of Text\AI\AI_SpaceStor
     ai_text_to_analyse = f.read()
 
 
-def evaluate_text_features(input_text):
-    """Evaluates numeric values for each of the following features; perplexity, burstiness, sentiment analysis,
-    Flesch-Kincaid readability score, Coleman-Liau readability index, average sentence length and vocabulary richness
-    in that exact order."""
-    feature_scores = []
+class CalcFeatures:
 
-    # Perplexity and Burstiness
-    ppl_burstiness = perplexity.PerplexityBurstiness.process_text_ppl_burstiness(input_text)
-    feature_scores.append(ppl_burstiness['avg_text_ppl'])
-    feature_scores.append(ppl_burstiness['text_burstiness'])
+    def __init__(self):
+        pass
 
-    # Sentiment Analysis
-    sentiment_score = sentimentAnalysis.SentimentAnalysis.perform_average_sentiment_analysis(input_text)
-    feature_scores.append(sentiment_score)
+    @staticmethod
+    def evaluate_text_features(input_text):
+        """Evaluates numeric values for each of the following features; perplexity, burstiness, sentiment analysis,
+        Flesch-Kincaid readability score, Coleman-Liau readability index, average sentence length and vocabulary richness
+        in that exact order."""
+        feature_scores = []
 
-    # Flesch-Kincaid Readability Score
-    fk_score = sentenceAnalysis.SentenceAnalysis.calculate_flesch_kincaid(input_text)
-    feature_scores.append(fk_score)
+        # Perplexity and Burstiness
+        ppl_burstiness = perplexity.PerplexityBurstiness.process_text_ppl_burstiness(input_text)
+        feature_scores.append(ppl_burstiness['avg_text_ppl'])
+        feature_scores.append(ppl_burstiness['text_burstiness'])
 
-    # Coleman-Liau Readability Score
-    cl_score = sentenceAnalysis.SentenceAnalysis.calculate_coleman_liau(input_text)
-    feature_scores.append(cl_score)
+        # Sentiment Analysis
+        sentiment_score = sentimentAnalysis.SentimentAnalysis.perform_average_sentiment_analysis(input_text)
+        feature_scores.append(sentiment_score)
 
-    # Average Sentence Length
-    avg_sentence_length = sentenceAnalysis.SentenceAnalysis.average_sentence_length_words(input_text)
-    feature_scores.append(avg_sentence_length)
+        # Flesch-Kincaid Readability Score
+        fk_score = sentenceAnalysis.SentenceAnalysis.calculate_flesch_kincaid(input_text)
+        feature_scores.append(fk_score)
 
-    # Vocabulary Richness
-    v_rich = vocab_richness.VocabRichness.calculate_total_vocab_richness_no_stopwords(input_text)
-    feature_scores.append(v_rich)
+        # Coleman-Liau Readability Score
+        cl_score = sentenceAnalysis.SentenceAnalysis.calculate_coleman_liau(input_text)
+        feature_scores.append(cl_score)
 
-    return feature_scores
+        # Average Sentence Length
+        avg_sentence_length = sentenceAnalysis.SentenceAnalysis.average_sentence_length_words(input_text)
+        feature_scores.append(avg_sentence_length)
+
+        # Vocabulary Richness
+        v_rich = vocab_richness.VocabRichness.calculate_total_vocab_richness_no_stopwords(input_text)
+        feature_scores.append(v_rich)
+
+        return feature_scores
 
 
-print(evaluate_text_features(human_text_to_analyse))
+print(CalcFeatures.evaluate_text_features(human_text_to_analyse))
