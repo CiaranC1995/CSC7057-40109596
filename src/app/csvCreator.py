43ffb955f1c1998186af8828bd13c417fe12535d
Ciaran Casey <ccasee@hotmail.co.uk>
2023-06-12 17:33:58 +0100

    Further changes to the function for creating a CSV file

--- src/app/csvCreator.py
+++ src/app/csvCreator.py
@@ -5,29 +5,36 @@ from src.ClassificationMetrics.TextPreprocessor import preprocessor as pp
 with open(r'C:\Users\ccase\Desktop\Dissertation\Examples of Text\Human\Human_HarryPotter.txt') as f:
     text_to_analyse = f.read()
 
+with open(r'C:\Users\ccase\Desktop\Dissertation\Examples of Text\AI\AI_SpaceStory.txt') as f:
+    text_to_analyse_1 = f.read()
 
-def process_quotation_marks(input_text):
-    return input_text.replace('"', '""')
+with open(r'C:\Users\ccase\Desktop\Dissertation\Examples of Text\AI\AI_AgileSoftwareDevelopment.txt') as f:
+    text_to_analyse_2 = f.read()
+
+with open(r'C:\Users\ccase\Desktop\Dissertation\Examples of Text\AI\AI_CiaranSoftwareEngineer.txt') as f:
+    text_to_analyse_3 = f.read()
+
+with open(r'C:\Users\ccase\Desktop\Dissertation\Examples of Text\AI\AI_ChatGPToutput.txt') as f:
+    text_to_analyse_4 = f.read()
+
+with open(r'C:\Users\ccase\Desktop\Dissertation\Examples of Text\AI\AI_BraveNewWorldEssay.txt') as f:
+    text_to_analyse_5 = f.read()
+
+unprocessedTexts = [text_to_analyse, text_to_analyse_1, text_to_analyse_2, text_to_analyse_3, text_to_analyse_4,
+                    text_to_analyse_5]
+
+processed_texts = [pp.TextPreprocessor.remove_new_line_chars(text) for text in unprocessedTexts]
+
+data_to_write = [[processed_texts[0], 0], [processed_texts[1], 1], [processed_texts[2], 1], [processed_texts[3], 1],
+                 [processed_texts[4], 1], [processed_texts[5], 1]]
 
 
-processed_text = pp.TextPreprocessor.remove_new_line_chars(text_to_analyse)
-csv_text = process_quotation_marks(processed_text)
-
-data_to_write = [[csv_text, 1]]
-print(data_to_write)
-print(data_to_write[0][1])
-
-
-def create_csv_file(input_data):
-    # Create the CSV file
+def create_and_add_to_csv_file(input_data):
     with open('test.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerows([['ID', 'Text', 'AI-Generated']])
-        text_id = 1
-        for text in range(len(input_data)):
-            writer.writerow([[text_id, input_data[text_id - 1][1], input_data[text_id - 1][0]]])
-            text_id += 1
-    print("CSV file 'test.csv' created successfully.")
+        writer.writerows([['Text', 'AI-Generated']])
+        writer.writerows(input_data)
+    print("Data added successfully.")
 
-# Call the function
-create_csv_file(data_to_write)
+
+create_and_add_to_csv_file(data_to_write)
