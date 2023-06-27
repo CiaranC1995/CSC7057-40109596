8d0f8d660ad416d44d11853f3d7a012f286f1029
Ciaran Casey <ccasee@hotmail.co.uk>
2023-06-27 09:51:32 +0100

    Adding logarithmic transformation to preprocessing script

--- src/app/newPreprocessingForTraining.py
+++ src/app/newPreprocessingForTraining.py
@@ -6,6 +6,7 @@ from nltk import word_tokenize, WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
 from nltk import pos_tag
+import numpy as np
 from scipy.stats import boxcox, yeojohnson
 
 start_time = time.time()
@@ -21,7 +22,7 @@ def process_entry(entry):
     return str(final_words)
 
 
-dataset = pd.read_csv("dataset_ready_to_split_and_transform.csv")
+dataset = pd.read_csv(r"C:\Users\ccase\Desktop\Dissertation\Datasets\dataset_ready_to_split_and_transform.csv")
 
 # Remove blank rows if any
 dataset['text_to_analyse'].dropna(inplace=True)
@@ -38,30 +39,27 @@ tag_map['R'] = wn.ADV
 word_lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 
-word = 'released'
-tag = 'VBD'
-print(f"Word: {word}, Tag: {tag}, Lemmatized Word: {word_lemmatizer.lemmatize(word, tag_map.get(tag[0], 'n'))}")
-
-
 # Apply the processing function to each entry in 'text_to_analyse' column
 dataset['text_final'] = dataset['text_to_analyse'].apply(process_entry)
 
 # new dataframes to store processed data columns
 # processed_dataframe_boxcox = pd.DataFrame()
 # processed_dataframe_yeojohnson = pd.DataFrame()
+processed_dataframe_logarithmic = pd.DataFrame()
 
 # dataframe to hold non-transformed ppl and burstiness values
-processed_dataframe = pd.DataFrame()
-processed_dataframe['text_to_analyse'] = dataset['text_final']
-processed_dataframe['perplexity'] = dataset['perplexity']
-processed_dataframe['burstiness'] = dataset['burstiness']
-processed_dataframe['AI Generated'] = dataset['AI Generated']
-processed_dataframe = processed_dataframe.sample(frac=1).reset_index(drop=True)
-processed_dataframe.to_csv('preprocessed_dataset_noTransformation.csv', index=False)
+# processed_dataframe = pd.DataFrame()
+# processed_dataframe['text_to_analyse'] = dataset['text_final']
+# processed_dataframe['perplexity'] = dataset['perplexity']
+# processed_dataframe['burstiness'] = dataset['burstiness']
+# processed_dataframe['AI Generated'] = dataset['AI Generated']
+# processed_dataframe = processed_dataframe.sample(frac=1).reset_index(drop=True)
+# processed_dataframe.to_csv('preprocessed_dataset_noTransformation.csv', index=False)
 
 # populate with preprocessed text
 # processed_dataframe_boxcox['text_to_analyse'] = dataset['text_final']
 # processed_dataframe_yeojohnson['text_to_analyse'] = dataset['text_final']
+processed_dataframe_logarithmic['text_to_analyse'] = dataset['text_final']
 
 # transform ppl and burstiness using boxcox (no negative values)
 # transformed_perplexity_boxcox, _ = boxcox(dataset['perplexity'])
@@ -71,23 +69,32 @@ processed_dataframe.to_csv('preprocessed_dataset_noTransformation.csv', index=Fa
 # transformed_perplexity_yeojohnson, _ = yeojohnson(dataset['perplexity'])
 # transformed_burstiness_yeojohnson, _ = yeojohnson(dataset['burstiness'])
 
+# transform using logarithmic method
+transformed_perplexity_logarithmic = np.log(dataset['perplexity'])
+transformed_burstiness_logarithmic = np.log(dataset['burstiness'])
+
 # populate dataframes with transformed values
 # processed_dataframe_boxcox['perplexity'] = transformed_perplexity_boxcox
 # processed_dataframe_boxcox['burstiness'] = transformed_burstiness_boxcox
 # processed_dataframe_yeojohnson['perplexity'] = transformed_perplexity_yeojohnson
 # processed_dataframe_yeojohnson['burstiness'] = transformed_burstiness_yeojohnson
+processed_dataframe_logarithmic['perplexity'] = transformed_perplexity_logarithmic
+processed_dataframe_logarithmic['burstiness'] = transformed_burstiness_logarithmic
 
 # populate dataframes with labels
 # processed_dataframe_boxcox['AI Generated'] = dataset['AI Generated']
 # processed_dataframe_yeojohnson['AI Generated'] = dataset['AI Generated']
+processed_dataframe_logarithmic['AI Generated'] = dataset['AI Generated']
 
 # randomise dataframes
 # processed_dataframe_boxcox = processed_dataframe_boxcox.sample(frac=1).reset_index(drop=True)
 # processed_dataframe_yeojohnson = processed_dataframe_yeojohnson.sample(frac=1).reset_index(drop=True)
+processed_dataframe_logarithmic = processed_dataframe_logarithmic.sample(frac=1).reset_index(drop=True)
 
 # output to csv files
 # processed_dataframe_boxcox.to_csv('preprocessed_dataset_boxcox.csv', index=False)
 # processed_dataframe_yeojohnson.to_csv('preprocessed_dataset_yeojohnson.csv', index=False)
+processed_dataframe_logarithmic.to_csv('preprocessed_dataset_logarithmic.csv', index=False)
 
 print("\nTime Elapsed: {:.2f}s".format(time.time() - start_time))
 print("Program finished executing at:", datetime.datetime.now())
